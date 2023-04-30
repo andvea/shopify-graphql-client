63551d1313973e55be278119e2284cc161ba460a
andvea <36574883+andvea@users.noreply.github.com>
2023-04-30 15:44:50 +0200

    First commit

---
+++ ShopifyGraphQL.js
@@ -0,0 +1,139 @@
+/*
+  Copyright 2023 Andrea Polverino (github.com/andvea)
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+'use strict';
+import fetch from 'node-fetch';
+
+export class ShopifyGraphQL {
+  constructor(configObject) {
+    this.fetch = fetch;
+    this.configObject = configObject;
+
+    if (!this.configObject.shopUrl) {
+      throw new Error('Missing Shop URL');
+    }
+    if (!this.configObject.shopApiKey) {
+      throw new Error('Missing Shop Api Key');
+    }
+
+    this.configObject.retryThrottles =
+      (this.configObject.retryThrottles!=undefined ?
+        this.configObject.retryThrottles :
+        true);
+  }
+
+  _syncDelay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  request(body, retries = 0) {
+    return new Promise((resolve, reject) => {
+      this.fetch(this.configObject.shopUrl, {
+        method: 'POST',
+        headers: {
+          'Accept': 'text/html',
+          'Content-Type': 'application/graphql',
+          'X-Shopify-Access-Token': this.configObject.shopApiKey,
+        },
+        body: body,
+      }).then(async (shopifyResult) => {
+        // Parse response's body
+        const JSON_RESULT = await shopifyResult.json();
+        // Check response's HTTP status code
+        switch (true) {
+          case shopifyResult.status >= 200 && shopifyResult.status <= 299:
+            // Ok, go ahead
+            break;
+          // case shopifyResult.status >= 500 && shopifyResult.status <= 599:
+          // case shopifyResult.status == 429:
+          default:
+            // Generic error, quit
+            return reject(new Error('', {
+              cause: {
+                status: shopifyResult.status,
+                errors: JSON_RESULT.errors,
+                userErrors: false,
+                cost: (JSON_RESULT.extensions ?
+                  JSON_RESULT.extensions.cost :
+                  null)}}));
+            break;
+        }
+        // Check response's errors body property
+        if (JSON_RESULT.errors!=undefined) {
+          let _isThrottled = false;
+          for (let i=0; i<JSON_RESULT.errors.length; i++) {
+            if (JSON_RESULT.errors[i].extensions.code == 'THROTTLED') {
+              _isThrottled = true;
+            }
+          }
+          // Retry with backoff if allowed by user, otherwise quit
+          if (_isThrottled && this.configObject.retryThrottles) {
+            // How long must I wait?
+            // (actualQueryCost is null for throttled requests)
+            const _x = JSON_RESULT.extensions.cost;
+            const _backoffMs =
+              Math.max(0,
+                  (_x.requestedQueryCost -
+                    _x.throttleStatus.currentlyAvailable) /
+                  _x.throttleStatus.restoreRate) * 1000;
+            // Wait for backoff time..
+            await this._syncDelay(_backoffMs);
+            // ..and retry the request!
+            return this.request(body, retries+1)
+                .then((s) => resolve(s))
+                .catch((r) => reject(r));
+          } else {
+            return reject(new Error('', {
+              cause: {
+                status: (_isThrottled ? 'throttled' : shopifyResult.status),
+                errors: JSON_RESULT.errors,
+                userErrors: false,
+                cost: (JSON_RESULT.extensions ?
+                  JSON_RESULT.extensions.cost :
+                  null),
+              }}));
+          }
+        }
+        // Check response's userErrors body property
+        const JSON_RESULT_FIRST_KEY = Object.keys(JSON_RESULT.data)[0];
+        if (
+          typeof JSON_RESULT.data[JSON_RESULT_FIRST_KEY] === 'object' &&
+          JSON_RESULT.data[JSON_RESULT_FIRST_KEY] != null &&
+          JSON_RESULT.data[JSON_RESULT_FIRST_KEY].userErrors &&
+          JSON_RESULT.data[JSON_RESULT_FIRST_KEY].userErrors.length > 0
+        ) {
+          return reject(new Error('', {
+            cause: {
+              status: shopifyResult.status,
+              errors: false,
+              userErrors: JSON_RESULT.data[JSON_RESULT_FIRST_KEY].userErrors,
+              cost: (JSON_RESULT.extensions ?
+                JSON_RESULT.extensions.cost :
+                null),
+            }}));
+        }
+
+        return resolve({
+          status: 'executed',
+          response: JSON_RESULT,
+          retries: retries,
+        });
+      }).catch((shopifyError) => {
+        return reject(new Error('', {cause: shopifyError}));
+      });
+    });
+  }
+}
---
+++ test/index.spec.js
@@ -0,0 +1,141 @@
+import supertest from 'supertest';
+import assert from 'node:assert';
+import * as dotenv from 'dotenv';
+import {ShopifyGraphQL} from '../ShopifyGraphQL.js';
+
+dotenv.config({
+  path: './.env.test'
+});
+
+var shopifyGraphQL =
+  new ShopifyGraphQL({
+    timeout: 2,
+    shopUrl: 'https://'+process.env.SHOP_MYSHOPIFY_DOMAIN+'/admin/api/2023-04/graphql.json',
+    shopApiKey: process.env.SHOP_API_KEY,
+    retryThrottles: false
+  });
+
+describe('GraphQL Errors', function() {
+  this.timeout(15000);
+
+  it('Succesfull request', (done) => {
+    shopifyGraphQL.request(`{ 
+      shop { 
+        id 
+      } 
+    }`, false, false)
+      .then((v) => {
+        done();
+      })
+      .catch((reqErr) => {
+        done(new Error(reqErr));
+      })
+  });
+
+  it('Reject if response has errors', (done) => {
+    shopifyGraphQL.request(`{ 
+      _customer(id: "gid://shopify/Customer/xxx") { 
+        createdAt 
+      } 
+    }`, false, false)
+      .then((v) => {
+        done(new Error('Malformed body passed'));
+      })
+      .catch((err) => {
+        if (!err.cause.userErrors && err.cause.errors) {
+          done();
+        } else {
+          done(new Error('Errors param is not defined'));
+        }
+      })
+  });
+
+  it('Reject if response has userErrors', (done) => {
+    shopifyGraphQL.request(`mutation { 
+      webhookSubscriptionDelete(id: "gid://shopify/WebhookSubscription/xxx") { 
+        userErrors { 
+          field 
+          message 
+        } 
+      } 
+    }`, false, false)
+      .then((v) => {
+        done(new Error('Malformed mutation input passed'));
+      })
+      .catch((err) => {
+        if (err.cause.userErrors && !err.cause.errors) {
+          done();
+        } else {
+          done(new Error('userErrors param is not defined'));
+        }
+      })
+  });
+
+  it('At least one request throttled', (done) => {
+    var throttlingPromises = [];
+    for (var i=0; i<100; i++) {
+      throttlingPromises.push(
+        shopifyGraphQL.request(`{ 
+          shop{ 
+            myshopifyDomain 
+            alerts{ action{ title } } 
+            currencySettings(first:10){ edges{ node{ currencyName } } } 
+            storefrontAccessTokens(first:10){ edges{ node { createdAt } } } 
+            privateMetafields(first:10){ edges{ node{ id } } } 
+            metafields(first:10){ edges{ node{ id } } } 
+          } 
+        }`, false, false));
+    }
+
+    Promise.all(throttlingPromises).then((success) => {
+      done(new Error('No throttled requests'));
+    }).catch((err) => {
+      if (err.cause.status=='throttled') {
+        done();
+      } else {
+        done(new Error('No throttled requests'));
+      }
+    });
+  });
+
+  it('Automatically retry throttled requests', (done) => {
+    shopifyGraphQL =
+      new ShopifyGraphQL({
+        timeout: 2,
+        shopUrl: 'https://'+process.env.SHOP_MYSHOPIFY_DOMAIN+'/admin/api/2023-04/graphql.json',
+        shopApiKey: process.env.SHOP_API_KEY,
+        retryThrottles: true
+      });
+
+    var throttlingPromises = [];
+    for (var i=0; i<70; i++) {
+      throttlingPromises.push(
+        shopifyGraphQL.request(`{ 
+          shop{ 
+            myshopifyDomain 
+            alerts{ action{ title } } 
+            currencySettings(first:10){ edges{ node{ currencyName } } } 
+            storefrontAccessTokens(first:10){ edges{ node { createdAt } } } 
+            privateMetafields(first:10){ edges{ node{ id } } } 
+            metafields(first:10){ edges{ node{ id } } } 
+          } 
+        }`, false, false));
+    }
+
+    Promise.all(throttlingPromises).then((success) => {
+      var _check = true;
+      for (var i=0; i<success.length; i++) {
+        if (success[i].response.data.shop.myshopifyDomain!=process.env.SHOP_MYSHOPIFY_DOMAIN) {
+          _check = false;
+        }
+      }
+      if (_check) {
+        done();
+      } else {
+        done(new Error('An error occurred with one or more requests'));
+      }
+    }).catch((promiseError) => {
+      done(new Error(promiseError));
+    });
+  });
+});
